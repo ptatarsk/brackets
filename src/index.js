220f2894745245ce4004dd6255adb841331c16f0
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-21 21:31:16 +0200

    feat: done Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  //your solution
+  bracketsConfig = bracketsConfig.map((config) => config.join(''));
+  for (let i = 0; i < bracketsConfig.length;) {
+    if (str.includes(bracketsConfig[i])) {
+      str = str.replace(bracketsConfig[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  return str ? false : true;
 }
